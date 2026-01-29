c40d6b2c6c6bddd5f4f01b6dca0d1c4978d7fb05
Sander de Gans <sander.degans@nxp.com>
2026-01-29 15:49:53 +0100

    Updates

--- assets/Header.stories-GPltmDdI.js
+++
@@ -1,7 +0,0 @@
-import{H as a}from"./Header-n6L65cfY.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-CX7HPOrl.js";import"./preload-helper-PPVm8Dsz.js";import"./Button-DYyBYPeH.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,p={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,p as default};
---
+++ assets/formatter-EIJCOSYU-DRZwdbVY.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-B3OSios1.js";import{b as t}from"./iframe-C7RJzklU.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CWQ2YXFj.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
