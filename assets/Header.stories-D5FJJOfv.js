cc97578752516ec6be5938b5e0410aa0921adc46
Sander de Gans <sander.degans@nxp.com>
2026-01-29 16:37:36 +0100

    Updates

--- assets/Header.stories-D5FJJOfv.js
+++
@@ -1,7 +0,0 @@
-import{H as a}from"./Header-JPDSUyto.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-DTwLFOGa.js";import"./preload-helper-PPVm8Dsz.js";import"./Button-_y8QM9b4.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,p={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,p as default};
